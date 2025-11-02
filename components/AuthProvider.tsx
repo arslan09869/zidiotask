54509fe36d5d0a7a72a931611e15f570c4da71db
Arslan09869 <arslan09869@gmail.com>
2025-11-02 15:04:34 +0530

    fix

--- components/AuthProvider.tsx
+++ components/AuthProvider.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import { Auth0Provider } from "@auth0/auth0-react";
 
 function AuthProvider({ children }: { children: React.ReactNode }) {
@@ -8,7 +8,7 @@ function AuthProvider({ children }: { children: React.ReactNode }) {
         domain="dev-rcfqfqv2x06cwc56.us.auth0.com"
         clientId="zOLVFn0FDRPOTmsoARsZwd57y7esOWtZ"
         authorizationParams={{
-          redirect_uri: window.location.origin,
+          redirect_uri: process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI!,
         }}
       >
         {children}
